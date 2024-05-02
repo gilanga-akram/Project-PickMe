0e58a53f313c2c251ccd68520e08218b95e5c4be
Gilang akram <gilang.akram1@gmail.com>
2024-05-02 15:03:48 +0700

    2 commit

--- Scripts/TC-001 Login/Login Admin Username Salah/Script1714635174695.groovy
+++ Scripts/TC-001 Login/Login Admin Username Salah/Script1714635174695.groovy
@@ -18,21 +18,17 @@ import internal.GlobalVariable as GlobalVariable
 import org.openqa.selenium.Keys as Keys
 import com.kms.katalon.core.testobject.ConditionType as ConditionType
 
-WebUI.openBrowser('')
-
-WebUI.navigateToUrl('https://dev.admin-pickme.metrodataacademy.id/login')
+WebUI.openBrowser('https://dev.admin-pickme.metrodataacademy.id/login/')
 
 WebUI.maximizeWindow()
 
 WebUI.click(new TestObject().addProperty('xpath', ConditionType.EQUALS, '//input[@id=\'username\']'))
 
-WebUI.setText(new TestObject().addProperty('xpath', ConditionType.EQUALS, '//input[@id=\'username\']'), 'superadmin')
+WebUI.setText(new TestObject().addProperty('xpath', ConditionType.EQUALS, '//input[@id=\'username\']'), 'Superadmin')
 
 WebUI.click(new TestObject().addProperty('xpath', ConditionType.EQUALS, '//input[@id=\'password\']'))
 
 WebUI.setEncryptedText(new TestObject().addProperty('xpath', ConditionType.EQUALS, '//input[@id=\'password\']'), 'o+tS4OuGt32s9ezZj287yw==')
 
 WebUI.click(new TestObject().addProperty('xpath', ConditionType.EQUALS, '//button[@type=\'submit\']'))
-
-WebUI.closeBrowser()
 
---
+++ Test Suites/TC-001 Login/Login Admin Berhasil.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Login Admin Berhasil</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>d75e34fe-7020-4164-b733-884d222a5245</testSuiteGuid>
+   <testCaseLink>
+      <guid>b1b6434f-bd68-43ec-919a-cc500342b59b</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TC-001 Login/Login Admin Berhasil</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
